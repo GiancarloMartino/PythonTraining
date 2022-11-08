e993a5f255f3efb0ea587c83af9a2dddd6a45965
Giancarlo Martino <gianmart83@gmail.com>
2022-11-08 17:58:26 +0100

    variables and strings added

---
+++ PythonSintaxElements/variables_py.py
@@ -0,0 +1,15 @@
+_auto = 23
+auto = 24
+
+AUTO = 25
+
+AUTO = 3629383
+
+numero_di_auto = 23
+tipo_di_auto = "Mercedes classe A"
+
+print(auto)
+print(_auto)
+print(AUTO)
+print(numero_di_auto)
+print(tipo_di_auto)
