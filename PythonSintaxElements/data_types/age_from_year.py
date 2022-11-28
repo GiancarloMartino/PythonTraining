7f7d8a24c472dcf2dc097495b38e783258b0dd30
Giancarlo Martino <gianmart83@gmail.com>
2022-11-28 07:19:31 +0100

    age_from_year added

---
+++ PythonSintaxElements/data_types/age_from_year.py
@@ -0,0 +1,8 @@
+print('I calculate your age from the year of your birth')
+
+year_of_birth = int(input('When are you born? \nPlease type the year: '))
+
+current_year = int(input('Please type the current year: '))
+age_from_year = current_year - year_of_birth
+
+print(f'your age is: {age_from_year}')
