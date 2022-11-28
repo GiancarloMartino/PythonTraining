86ba5dc402efe51924275d0ed0532ab7fc1ce97a
Giancarlo Martino <gianmart83@gmail.com>
2022-11-28 06:30:38 +0100

    basic_data_types.py added

---
+++ PythonSintaxElements/data_types/basic_data_types.py
@@ -0,0 +1,35 @@
+# print is a function. It prints in terminal the value of the argument
+print(1)
+print(1.245)
+print("stringa di caratteri alfabetici")
+print(True)
+print([2, 3, 5, 7, 11, 13])
+print({2, 3, 5, 7, 11, 13})
+print({'prima_chiave': 'primo_valore', 'seconda_chiave': 'secondo_valore'})
+
+
+# type() tells us the data type of the value we passed
+print(type(1))
+print(type(1.245))
+print(type("stringa di caratteri alfabetici"))
+print(type(True))
+print(type([2, 3, 5, 7, 11, 13]))
+print(type({2, 3, 5, 7, 11, 13}))
+print(type({'prima_chiave': 'primo_valore', 'seconda_chiave': 'secondo_valore'}))
+
+
+integer_type = 1
+float_type = 1.245
+string_chars_type = "stringa di caratteri alfabetici"
+boolean_type = True
+list_type = [2, 3, 5, 7, 11, 13]  # array
+set_type = {2, 3, 5, 7, 11, 13}  # set
+dictionary_type = {'prima_chiave': 'primo_valore', 'seconda_chiave': 'secondo_valore'}  # dictionary
+
+print(type(integer_type))
+print(type(float_type))
+print(type(string_chars_type))
+print(type(boolean_type))
+print(type(list_type))
+print(type(set_type))
+print(type(dictionary_type))
