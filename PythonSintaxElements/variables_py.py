070af3e2e0d139206dde3d2507597961a9c00875
Giancarlo Martino <gianmart83@gmail.com>
2022-11-28 06:39:38 +0100

    variables simple refactored

--- PythonSintaxElements/variables_py.py
+++ PythonSintaxElements/variables_py.py
@@ -1,15 +1,16 @@
 _auto = 23
+print(_auto)
 auto = 24
+print(auto)
 
 AUTO = 25
+print(AUTO)
 
 AUTO = 3629383
+print(AUTO)
 
 numero_di_auto = 23
 tipo_di_auto = "Mercedes classe A"
 
-print(auto)
-print(_auto)
-print(AUTO)
 print(numero_di_auto)
 print(tipo_di_auto)
